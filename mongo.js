dee03505a8e3941d265a1a4c0c76e6370a343a4e
momenneko <momenneko23@gmail.com>
2018-01-17 04:12:57 +0900

    moved environment variables to .env and updated README

--- mongo.js
+++ mongo.js
@@ -1,7 +1,8 @@
+var dotenv = require('dotenv').config();
 var mongodb = require('mongodb');
 var users;
 
-mongodb.MongoClient.connect("mongodb://localhost:27017/petdb", function(err, db) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
 	if (err) { return console.dir(err);}
 	console.log("connected to db");
 	users = db.collection('users');
